c307df30bb9eb66722bc84bdf6fdcefe19d12f0d
Bryce-Davidson <lifebryce@gmail.com>
2021-12-09 12:57:06 -0800

    Removed node_modules

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
+require("dotenv").config();
+
 const url =
   "https://www.memoryexpress.com/Category/VideoCards?InventoryType=InStock&Inventory=BCVIC1";
 const card_class = ".c-shca-icon-item__body-name a";
 const key_words = ["RTX", "rtx", "GTX", "gtx"];
+
+console.log(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+
+// const client = require("twilio")(
+//   process.env.TWILIO_ACCOUNT_SID,
+//   process.env.TWILIO_AUTH_TOKEN
+// );
+
+client.messages
+  .create({
+    from: process.env.TWILIO_PHONE_NUMBER,
+    to: process.env.CELL_PHONE_NUMBER,
+    body: "You just sent an SMS from Node.js using Twilio!",
+  })
+  .then((message) => console.log(message.sid));
 
 function scrape(seconds) {
   let count = 0;
@@ -32,4 +49,4 @@ function scrape(seconds) {
   }, seconds * 1000);
 }
 
-scrape(3);
+// scrape(3);
