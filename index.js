925351b424e343f8c021aabb5ea413242389485c
Bryce-Davidson <lifebryce@gmail.com>
2021-12-09 22:19:27 -0800

    Changed index code

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 const twilio = require("twilio");
-const moment = require("moment-timezone");
 const http = require("http");
 
 if (process.env.NODE_ENV !== "production") {
@@ -25,70 +24,52 @@ const numbers = process.env.NUMBERS.split(",");
 
 let count = 0;
 function scrape() {
-  const currentTime = moment().tz("America/Vancouver");
-  console.log(currentTime);
-  const current_time_format = currentTime.format("h:mm A");
-  console.log(currentTime);
+  console.log("Scraping...");
+  axios
+    .get(url)
+    .then((response) => {
+      const $ = cheerio.load(response.data);
+      const cards = $(card_class)
+        .filter((i, el) => {
+          $(el).find("span").remove();
+          const text = $(el).text().trim();
+          return key_words.some((key) => text.includes(key));
+        })
+        .map((i, el) => {
+          return $(el).text().trim();
+        })
+        .get();
+      count++;
+      console.log({ count, time: current_time_format, cards });
 
-  console.log("Checking time...");
-
-  const start_time = moment(currentTime).set({ hour: 10, minute: 0 });
-  const end_time = moment(currentTime).set({ hour: 20, minute: 0 });
-
-  console.log(currentTime, start_time, end_time);
-  if (moment(currentTime).isBetween(start_time, end_time)) {
-    console.log("Scraping...");
-    axios
-      .get(url)
-      .then((response) => {
-        const $ = cheerio.load(response.data);
-        const cards = $(card_class)
-          .filter((i, el) => {
-            $(el).find("span").remove();
-            const text = $(el).text().trim();
-            return key_words.some((key) => text.includes(key));
-          })
-          .map((i, el) => {
-            return $(el).text().trim();
-          })
-          .get();
-        count++;
-        console.log({ count, time: current_time_format, cards });
-
-        return Promise.all(
-          numbers.map((number) => {
-            return client.messages.create({
-              body: `
+      return Promise.all(
+        numbers.map((number) => {
+          return client.messages.create({
+            body: `
               Update: ${count}\nStock: ${
-                cards.length
-              } cards\n${current_time_format}\n\n${cards.join("\n\n")}`,
-              to: number,
-              from: "whatsapp:+14155238886",
-            });
-          })
-        );
-      })
-      .then((messages) => {
-        messages.forEach((message) => {
-          console.log(`Payload sent to ${message.to.split(":")[1]}`);
-        });
-      })
-      .catch((error) => {
-        console.log(error);
+              cards.length
+            } cards\n${current_time_format}\n\n${cards.join("\n\n")}`,
+            to: number,
+            from: "whatsapp:+14155238886",
+          });
+        })
+      );
+    })
+    .then((messages) => {
+      messages.forEach((message) => {
+        console.log(`Payload sent to ${message.to.split(":")[1]}`);
       });
-  } else {
-    console.log("Not time yet...");
-  }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
-const minutes = 20;
 
+const minutes = 20;
 var server = http.createServer((req, res) => {});
 
 server.listen(process.env.PORT || 80, () => {
   console.log("Listening on port 80");
   scrape();
   setInterval(scrape, minutes * 1000 * 60);
-  //   setInterval(() => {
-  //     console.log("Server is running...");
-  //   }, 1000);
 });
