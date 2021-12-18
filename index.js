6dc5130c6e84a35cdbf5f8bd1170c1c0af5c3106
Bryce-Davidson <lifebryce@gmail.com>
2021-12-17 16:40:26 -0800

    Added memory

--- index.js
+++ index.js
@@ -29,61 +29,58 @@ const key_words = [
   "3090",
 ];
 
-let count = 0;
-let prev_cards = "";
+let prev_cards;
 
 async function scrape(next_job) {
   console.log("Scraping...");
-  return axios
-    .get(url)
-    .then((response) => {
-      const $ = cheerio.load(response.data);
-      const cards = $(card_class)
-        .filter((i, el) => {
-          $(el).find("span").remove();
-          const text = $(el).text().trim();
-          return key_words.some((key) => text.includes(key));
-        })
-        .map((i, el) => {
-          return $(el).text().trim();
-        })
-        .get();
+  axios.get(url).then((response) => {
+    const $ = cheerio.load(response.data);
+    const cards = $(card_class)
+      .filter((i, el) => {
+        $(el).find("span").remove();
+        const text = $(el).text().trim();
+        return key_words.some((key) => text.includes(key));
+      })
+      .map((i, el) => {
+        return $(el).text().trim();
+      })
+      .get();
 
-      const send_text = prev_cards == cards.join(",") ? false : true;
-      console.log({ count, prev_cards, cards });
-      prev_cards = cards.join(",");
+    const send_text = prev_cards !== cards.join(",");
+    console.log({ prev_cards, cards: cards.join(",") });
+    prev_cards = cards.join(",");
+    console.log({ prev_cards, cards: cards.join(",") });
 
-      if (send_text) {
-        return Promise.all(
-          numbers.map((number) => {
-            return client.messages.create({
-              body: `
+    if (send_text) {
+      return Promise.all(
+        numbers.map((number) => {
+          return client.messages.create({
+            body: `
             \nStock: ${cards.length}\n\n${cards
-                .map((card) => {
-                  return card.split("GB")[0] + "GB";
-                })
-                .join("\n\n")}`.replace(" ", "\u{0020}"),
-              messagingServiceSid: process.env.TWILIO_MESSAGING_SERVICE_SID,
-              to: number,
-            });
-          })
-        );
-      }
-    })
-    .then((messages) => {
-      messages.forEach((message) => {
-        console.log(`Payload sent to ${message.to}`);
+              .map((card) => {
+                return card.split("GB")[0] + "GB";
+              })
+              .join("\n\n")}`.replace(" ", "\u{0020}"),
+            messagingServiceSid: process.env.TWILIO_MESSAGING_SERVICE_SID,
+            to: number,
+          });
+        })
+      ).then((messages) => {
+        messages.forEach((message) => {
+          console.log(`Payload sent to ${message.to}`);
+        });
       });
-    });
+    }
+  });
 }
 
 const job = new CronJob({
-  cronTime: "32,2 10-19 * * *",
+  cronTime: "*/10 * 10-19 * * *",
   onTick: async (onComplete) => {
     if (job.taskRunning) return;
     job.taskRunning = true;
 
-    const next_job = job.nextDates().format("MMM DD, HH:mm A");
+    const next_job = job.nextDates().format("MMM DD, HH:mm-ss[s] A");
     await scrape(next_job).then(onComplete).catch(console.log);
 
     job.taskRunning = false;
@@ -99,7 +96,7 @@ const job = new CronJob({
 });
 
 const health_check = new CronJob({
-  cronTime: "*/5 * * * *",
+  cronTime: "*/10 * * * *",
   onTick: () => {
     console.log("Server is Running...");
     console.log(`Next Job: ${job.nextDates().format("MMM DD, HH:mm A")}`);
